04283be19fff5cb08b8b47645fa2b3cad4108309
Martin Costa Daddato <costadaddato@gmail.com>
2023-05-05 19:48:06 -0300

    Pre entrega Martin Costa Daddato

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import CartWidget from "./CartWidget";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <a href="#">Logo</a>
+      <a href="#">Logo MCD</a>
       <div>
         <a href="#"><button>Productos</button></a>
         <a href="#"><button>Destacado</button></a>
